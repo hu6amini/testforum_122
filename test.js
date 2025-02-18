ef46eac2b79aeff2a2372ec1f4150ffb8bbc3c8e
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-18 18:09:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-
-  $(document).ready(function() {
-    $("#Post").markItUp(markItUpSettings); // Use the default settings from set.min.js
-  });
+var markItUpSettings = {
+    nameSpace: "default",
+    onShiftEnter: {keepDefault:false, replaceWith:'\n\n'},
+    markupSet: [
+      {name:'Bold', key:'B', openWith:'[b]', closeWith:'[/b]'},
+      {name:'Italic', key:'I', openWith:'[i]', closeWith:'[/i]'},
+      {name:'Underline', key:'U', openWith:'[u]', closeWith:'[/u]'},
+      {separator:'---------------' },
+      {name:'Link', key:'L', openWith:'[url=[![Link]!]]', closeWith:'[/url]', placeHolder:'Your text here...'}
+    ]
+  };
