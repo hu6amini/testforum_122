c547586286b1192e494ffc8f6b98e0c48498db17
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-18 18:50:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-  $(document).ready(function() {
-    new FroalaEditor('#Post', {
-      heightMin: 100,  // Adjust height as needed
-      heightMax: 600,
-      toolbarButtons: ['bold', 'italic', 'underline', 'insertImage', 'insertLink', 'undo', 'redo'], // Customize toolbar
-      imageUpload: false // Disable image upload if needed
-    });
-  });
+
