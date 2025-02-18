fa154f45e512c9eaf568204a4f40fce7a52ba52a
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-18 18:55:25 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-
+$(document).ready(function() {
+    $("#Post").sceditor({
+        format: "bbcode",
+        style: "https://cdnjs.cloudflare.com/ajax/libs/sceditor/3.2.0/themes/modern.min.css",
+        toolbar: "bold,italic,underline|left,center,right|quote,code|image,link,unlink|source",
+        resizeEnabled: true,
+    });
+});
