01e065afd0765125045fdddcb1dde0f5ea00a0c7
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-24 08:14:50 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
     $("#Post").sceditor({
+    var editor = $("#Post").sceditor({
         format: "bbcode",
         style: "https://cdnjs.cloudflare.com/ajax/libs/sceditor/3.2.0/themes/content/default.min.css",
         toolbar: "bold,italic,underline|left,center,right|quote,code|image,link,unlink|source",
-        resizeEnabled: true
+        resizeEnabled: true,
     });
-});
+        });
