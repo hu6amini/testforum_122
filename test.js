706411e7716af22ac1ec60177aaf029c232fa719
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-24 10:50:47 +0200

    Update test.js

--- test.js
+++ test.js
@@ -3,19 +3,6 @@ $(document).ready(function () {
         format: "bbcode",
         style: "https://cdnjs.cloudflare.com/ajax/libs/sceditor/3.2.0/themes/content/default.min.css",
         toolbar: "bold,italic,underline|left,center,right|quote,code|image,link,unlink|source",
-        resizeEnabled: true,
-        icons: null // Disable default icons so we can use custom ones
+        resizeEnabled: true
     });
-
-    // Wait for SCEditor to initialize before replacing icons
-    setTimeout(function () {
-        $(".sceditor-button-bold div").html('<i class="fa-regular fa-bold"></i>');
-        $(".sceditor-button-italic div").html('<i class="fa-regular fa-italic"></i>');
-        $(".sceditor-button-underline div").html('<i class="fa-regular fa-underline"></i>');
-        $(".sceditor-button-code div").html('<i class="fa-solid fa-code"></i>');
-        $(".sceditor-button-image div").html('<i class="fa-regular fa-image"></i>');
-        $(".sceditor-button-link div").html('<i class="fa-solid fa-link"></i>');
-        $(".sceditor-button-unlink div").html('<i class="fa-solid fa-unlink"></i>');
-        $(".sceditor-button-quote div").html('<i class="fa-solid fa-quote-left"></i>');
-    }, 100);
 });
