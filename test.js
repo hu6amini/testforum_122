843de856a7def9f2995a1cb0d082cf2151572cca
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-24 11:46:16 +0200

    Update test.js

--- test.js
+++ test.js
@@ -5,7 +5,10 @@ $(document).ready(function () {
     $post.sceditor({
         format: "bbcode",
         style: "https://cdnjs.cloudflare.com/ajax/libs/sceditor/3.2.0/themes/content/default.min.css",
-        toolbar: "bold,italic,underline|left,center,right|quote,code|image,link,unlink|source",
+        toolbar: "bold,italic,underline,strikethrough|subscript,superscript|left,center,right,justify|" +
+         "font,size,color,removeformat|cut,copy,paste|bulletlist,orderedlist,table|" +
+         "code,quote,horizontalrule|image,email,link,unlink,youtube|" +
+         "ltr,rtl|date,time|source,maximize",
         resizeEnabled: true,
         plugins: "bbcode",
         toolbarExclude: "source"
