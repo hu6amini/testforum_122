e9c7c1581b0a0738f911c35a14055aea0c62d589
jr <89549621+hu6amini@users.noreply.github.com>
2025-02-26 21:43:51 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,33 +1,38 @@
-window.onload = function () {
-    function extractImageSize(src, callback) {
-        let tempImg = new Image();
-        tempImg.src = src;
-        tempImg.onload = function () {
-            callback(tempImg.width, tempImg.height);
-        };
-    }
+function extractImageSize(src, callback) {
+    let tempImg = new Image();
+    tempImg.src = src;
+    tempImg.onload = function () {
+        callback(tempImg.width, tempImg.height);
+    };
+}
 
-    function prepareLazyImages() {
-        document.querySelectorAll(".color img[data-ffbsrc]:not(.lazyload)").forEach(img => {
-            let originalSrc = img.getAttribute("data-ffbsrc"); // Get the source from data-ffbsrc
-            if (originalSrc) {
-                extractImageSize(originalSrc, function (width, height) {
-                    // Set attributes for lazy loading
-                    img.setAttribute("data-src", originalSrc);
-                    img.setAttribute("src", "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + width + "' height='" + height + "' viewBox='0 0 " + width + " " + height + "'%3E%3C/svg%3E");
-                    img.setAttribute("width", width);
-                    img.setAttribute("height", height);
-                    img.setAttribute("decoding", "async"); // Add decoding attribute
-                    img.classList.add("lazyload"); // Add lazyload class
-                });
-            }
-        });
-    }
+function prepareLazyImages() {
+    document.querySelectorAll(".color img[data-ffbsrc]:not(.lazyload)").forEach(img => {
+        let originalSrc = img.getAttribute("data-ffbsrc"); // Get the source from data-ffbsrc
+        if (originalSrc) {
+            extractImageSize(originalSrc, function (width, height) {
+                // Set attributes for lazy loading
+                img.setAttribute("data-src", originalSrc);
+                img.setAttribute("src", "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + width + "' height='" + height + "' viewBox='0 0 " + width + " " + height + "'%3E%3C/svg%3E");
+                img.setAttribute("width", width);
+                img.setAttribute("height", height);
+                img.setAttribute("decoding", "async"); // Add decoding attribute
+                img.classList.add("lazyload"); // Add lazyload class
+            });
+        }
+    });
+}
 
-    // Initial call to prepare lazy images
-    prepareLazyImages();
+// Initial call to prepare lazy images
+prepareLazyImages();
 
-    // Observe changes in the document
-    const observer = new MutationObserver(prepareLazyImages);
-    observer.observe(document.body, { childList: true, subtree: true });
-};
+// Observe changes in the document
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach(mutation => {
+        if (mutation.type === "childList") {
+            prepareLazyImages(); // Reprocess images when new nodes are added
+        }
+    });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
