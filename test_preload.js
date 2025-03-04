bf729ec6532f9351698f9f48a5415405c7c2725a
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-04 08:03:53 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,21 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".color img").forEach(function (img) {
-        if (img.complete && img.naturalWidth && img.naturalHeight) {
-            var width = img.naturalWidth;
-            var height = img.naturalHeight;
-            var src = img.src;
-
-            // Only process if not already lazyloaded
-            if (!img.classList.contains("lazyload")) {
-                img.setAttribute("data-src", src);
-                img.setAttribute("width", width);
-                img.setAttribute("height", height);
-                img.setAttribute("decoding", "async");
-                img.classList.add("lazyload");
-
-                // Set placeholder SVG to avoid layout shifts
-                img.src = "data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='" + width + "' height='" + height + "' viewBox='0 0 " + width + " " + height + "'%3E%3C/svg%3E";
-            }
-        }
+  $(document).ready(function() {
+    $(".color .quote").readmore({
+      speed: 150,
+      collapsedHeight: 170,
+      moreLink: '<a href="#">Show More...</a>',
+      lessLink: '<a href="#">Show Less</a>'
     });
-});
+  });
