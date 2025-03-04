87bf4112d3b2da1d6485f3271070e48cb66593ec
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-04 21:21:07 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
   function applyReadmore(target) {
-    $(target).find(".quote").readmore({
-      speed: 150,
-      collapsedHeight: 170,
-      moreLink: '<a href="#">Show More...</a>',
-      lessLink: '',
-      afterToggle: function(trigger, element, expanded) {
-        if (expanded) {
-          $(trigger).remove(); // Removes the "Show More" button after expanding
-        }
-      }
-    });
+    $(target)
+      .find(".quote")
+      .not(".ve-content.color .quote") // Exclude .quote elements inside .ve-content.color
+      .readmore({
+        speed: 150,
+        collapsedHeight: 170,
+        moreLink: '<a href="#" class="quote-expand">Show More...</a>',
+        lessLink: "",
+        beforeToggle: function (trigger, element, expanded) {
+          if (!expanded) {
+            // Move the "Show More" link inside the .quote at the bottom
+            $(element).append($(trigger));
+          }
+        },
+        afterToggle: function (trigger, element, expanded) {
+          if (expanded) {
+            $(trigger).remove(); // Remove the "Show More" button after expanding
+          }
+        },
+      });
   }
 
-  // Apply Readmore.js to initial elements
+  // Apply Readmore.js to initial .color elements
   applyReadmore($(".color"));
 
   // MutationObserver to watch for dynamically added .color elements
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => {
+  const quoteObserver = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
         if ($(node).is(".color")) {
           applyReadmore($(node)); // Apply Readmore.js to newly added .color elements
         } else if ($(node).find(".color").length) {
@@ -29,5 +38,6 @@ $(document).ready(function() {
     });
   });
 
-  observer.observe(document.body, { childList: true, subtree: true });
+  // Start observing the document body for added nodes
+  quoteObserver.observe(document.body, { childList: true, subtree: true });
 });
