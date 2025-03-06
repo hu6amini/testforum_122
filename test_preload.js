730547dbcd162295b8c5cef257a28765dc0c9f4d
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-06 21:37:06 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,20 +1,15 @@
 $(document).ready(function() {
   function applyReadmore(target) {
     $(target).find(".quote").readmore({
-      speed: 382,
+      speed: 618,
       collapsedHeight: 170,
-      moreLink: '<a href="#">Show More...</a>',
-      lessLink: '',
+      moreLink: '<button type="button">Show More...</button>', // Use a button instead of an anchor
+      lessLink: '', // You can also customize the "Show Less" button if needed
       afterToggle: function(trigger, element, expanded) {
         if (expanded) {
           $(trigger).remove(); // Removes the "Show More" button after expanding
         }
       }
-    });
-
-    // Prevent default action of the "Show More" link
-    $(target).find(".quote").on("click", "a[href='#']", function(event) {
-      event.preventDefault(); // Stop the link from scrolling to the top
     });
   }
 
