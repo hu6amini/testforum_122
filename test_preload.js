b415ba88a7554bc4d00bd76a05addabfd7391616
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-06 18:55:38 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,19 +1,33 @@
- document.addEventListener('DOMContentLoaded', function() {
-      if (typeof Readmore === 'undefined') {
-        console.error('Readmore.js is not loaded. Please check the script source.');
-        return;
-      }
+$(document).ready(function() {
+    function applyReadmore(target) {
+      $(target).find(".quote").readmore({
+        speed: 382,
+        collapsedHeight: 170,
+        moreLink: '<a href="#">Show More...</a>',
+        lessLink: '',
+        afterToggle: function(trigger, element, expanded) {
+          if (expanded) {
+            $(trigger).remove(); // Removes the "Show More" button after expanding
+          }
+        }
+      });
+    }
 
-      var quotes = document.querySelectorAll('.color .quote');
-      quotes.forEach(function(quote) {
-        new Readmore(quote, {
-          speed: 100,
-          collapsedHeight: 100,
-          heightMargin: 16,
-          moreLink: '<a href="#">Read more</a>',
-          lessLink: '<a href="#">Close</a>',
-          embedCSS: true,
-          blockCSS: 'display: block; width: 100%;',
+    // Apply Readmore.js to initial elements
+    applyReadmore($(".color"));
+
+    // MutationObserver to watch for dynamically added .color elements
+    const quoteObserver = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          if ($(node).is(".color")) {
+            applyReadmore($(node)); // Apply Readmore.js to newly added .color elements
+          } else if ($(node).find(".color").length) {
+            applyReadmore($(node).find(".color")); // If a parent contains .color elements, apply Readmore.js
+          }
         });
       });
     });
+
+    quoteObserver.observe(document.body, { childList: true, subtree: true });
+  });
