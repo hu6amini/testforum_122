dc1a3e22c38875901016d902da5df04f1e0f03e6
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-06 21:52:51 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,38 +1,6 @@
-$(document).ready(function() {
-  function applyReadmore(target) {
-    // Apply Readmore.js to the target .quote element
-    $(target).readmore({
-      speed: 618,
-      collapsedHeight: 170,
-      moreLink: '<a href="#">Show More...</a>',
-      lessLink: '',
-      afterToggle: function(trigger, element, expanded) {
-        if (expanded) {
-          $(trigger).remove(); // Removes the "Show More" button after expanding
-        }
-      }
-    });
-  }
-
-  // Apply Readmore.js to initial .quote elements inside .color
-  $(".color .quote").each(function() {
-    applyReadmore($(this));
-  });
-
-  // MutationObserver to watch for dynamically added .color elements
-  const quoteObserver = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      mutation.addedNodes.forEach(node => {
-        // Check if the added node is a .color element or contains .color elements
-        if ($(node).is(".color") || $(node).find(".color").length) {
-          // Find .quote elements inside .color and apply Readmore.js to them
-          $(node).find(".color .quote").each(function() {
-            applyReadmore($(this));
-          });
-        }
-      });
+ $(document).ready(function() {
+    $('.quote').readmore({
+      speed: 382,
+      collapsedHeight: 170
     });
   });
-
-  quoteObserver.observe(document.body, { childList: true, subtree: true });
-});
