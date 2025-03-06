ed4d73facaecfa30b1f815498024cf7b1e96f866
jr <89549621+hu6amini@users.noreply.github.com>
2025-03-06 17:57:57 +0200

    Update test_preload.js

--- test_preload.js
+++ test_preload.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   function applyReadmore(target) {
-    $(target).find(".quote").readmore({
+    $(target).find(".color .quote").not(".ve-content.color .quote").readmore({
       speed: 382,
       collapsedHeight: 170,
       moreLink: '<a href="#">Show More...</a>',
@@ -13,17 +13,17 @@ $(document).ready(function() {
     });
   }
 
-  // Apply Readmore.js to initial elements
-  applyReadmore($(".color"));
+  // Apply Readmore.js to initial elements, explicitly excluding .ve-content.color .quote
+  applyReadmore(document);
 
   // MutationObserver to watch for dynamically added .color elements
   const quoteObserver = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
       mutation.addedNodes.forEach(node => {
         if ($(node).is(".color")) {
-          applyReadmore($(node)); // Apply Readmore.js to newly added .color elements
-        } else if ($(node).find(".color").length) {
-          applyReadmore($(node).find(".color")); // If a parent contains .color elements, apply Readmore.js
+          applyReadmore(node); // Apply Readmore.js to newly added .color elements
+        } else if ($(node).find(".color .quote").not(".ve-content.color .quote").length) {
+          applyReadmore(node); // If a parent contains .color elements, apply Readmore.js
         }
       });
     });
